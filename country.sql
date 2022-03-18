7a0f0e858b585fae7834fb03e8e3987533b4965c
Edgar-Uriel-Ramos-Ramirez <68761952+Edgar-Uriel-Ramos-Ramirez@users.noreply.github.com>
2022-03-17 21:11:28 -0600

    Add files via upload

---
+++ country.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS `country` (
+`id` int(11) NOT NULL,
+  `country_name` varchar(255) NOT NULL
+);
+
+INSERT INTO `country` (`id`, `country_name`) VALUES
+(1, 'Aguascalientes'),
+(2, 'Baja California'),
+(3, 'Baja California Sur'),
+(4, 'Campeche'),
+(5, 'Chiapas'),
+(6, 'Chihuahua'),
+(7, 'Coahuila'),
+(8, 'Colima'),
+(9, 'Ciudad de México / CDMX'),
+(10, 'Durango'),
+(11, 'Estado de Mexico'),
+(12, 'Guanajuato'),
+(13, 'Guerrero'),
+(14, 'Hidalgo'),
+(15, 'Jalisco'),
+(16, 'Michoacán'),
+(17, 'Morelos'),
+(18, 'Nayarit'),
+(19, 'Nuevo León'),
+(20, 'Oaxaca'),
+(21, 'Puebla'),
+(22, 'Querétaro'),
+(23, 'Quintana Roo'),
+(24, 'San Luis Potosí'),
+(25, 'Sinaloa'),
+(26, 'Sonora'),
+(27, 'Tabasco'),
+(28, 'Tamaulipas'),
+(29, 'Tlaxcala'),
+(30, 'Veracruz'),
+(31, 'Yucatán'),
+(32, 'Zacatecas');
